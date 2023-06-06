27c8d999cb800644fdd453206d848744fa56be81
tvspng <tvsantos@edu.unifor.br>
2023-06-06 15:33:45 -0300

    atualiza-api

--- server.js
+++ server.js
@@ -1,105 +1,83 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
-// Configurar o Express
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-// Configurar conexão com o MongoDB
-
-mongoose.connect("mongodb+srv://tvsantos:AQ6MUM2TWIu4Ap66@flores.wxzleyy.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  "mongodb+srv://thais:OsBrlxCRWKXAk8mF@cluster0.4mnwtae.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro de conexão com o banco de dados:'));
-db.once('open', function() {
-  console.log('Conectado ao banco de dados.');
+db.on(
+  "error",
+  console.error.bind(console, "Erro de conexão com o banco de dados:")
+);
+db.once("open", function () {
+  console.log("Conectado ao banco de dados.");
 });
 
-// Definir o modelo do usuário
 const UsuarioSchema = new mongoose.Schema({
   nome: String,
-  senha: String
+  senha: String,
 });
 
-const Usuario = mongoose.model('Usuario', UsuarioSchema);
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
 
-// Rota de registro de usuário
-app.post('/registro', (req, res) => {
+app.post("/registro", async (req, res) => {
   const { nome, senha } = req.body;
 
-  // Verificar se o usuário já existe no banco de dados
-  Usuario.findOne({ nome: nome }, (err, usuarioExistente) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Erro interno do servidor');
-    } else if (usuarioExistente) {
-      res.status(400).send('Usuário já registrado');
+  const usuarioExistente = await Usuario.findOne({ nome: nome });
+  if (usuarioExistente) {
+    res.status(400).send("Usuário já registrado");
+  } else {
+    const senhaCriptografada = bcrypt.hashSync(senha, 10);
+
+    const usuario = new Usuario({
+      nome: nome,
+      senha: senhaCriptografada,
+    });
+    const user = usuario.save();
+    if (user) {
+      res.send("Usuário registrado com sucesso");
     } else {
-      // Criptografar a senha antes de salvar no banco de dados
-      const senhaCriptografada = bcrypt.hashSync(senha, 10);
-
-      const usuario = new Usuario({
-        nome: nome,
-        senha: senhaCriptografada
-      });
-
-      usuario.save((err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Erro interno do servidor');
-        } else {
-          res.send('Usuário registrado com sucesso');
-        }
-      });
+      res.status(500).send("Erro interno do servidor");
     }
-  });
+  }
 });
 
 // Rota de login
-app.post('/login', (req, res) => {
+app.post("/login", async (req, res) => {
   const { nome, senha } = req.body;
 
   // Verificar se o usuário existe no banco de dados
-  Usuario.findOne({ nome: nome }, (err, usuario) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Erro interno do servidor');
-    } else if (!usuario) {
-      res.status(401).send('Credenciais inválidas');
+  const usuario = await Usuario.findOne({ nome });
+  if (!usuario) {
+    res.status(401).send("Credenciais inválidas");
+  } else {
+    // Verificar se a senha é válida
+    const senhaValida = bcrypt.compareSync(senha, usuario.senha);
+    if (!senhaValida) {
+      res.status(401).send("Credenciais inválidas");
     } else {
-      // Verificar se a senha é válida
-      const senhaValida = bcrypt.compareSync(senha, usuario.senha);
-      if (!senhaValida) {
-        res.status(401).send('Credenciais inválidas');
-      } else {
-        // Gerar um token JWT
-        const token = jwt.sign({ id: usuario._id, nome: usuario.nome }, 'chave-secreta-do-token', { expiresIn: '1h' });
+      // Gerar um token JWT
+      const token = jwt.sign(
+        { id: usuario._id, nome: usuario.nome, isAdmin: usuario?.isAdmin },
+        "change-of-mind",
+        { expiresIn: "1h" }
+      );
 
-        res.json({ token: token });
-      }
+      res.json({ token: token });
     }
-  });
+  }
 });
 
-// Middleware para verificar o token em rotas protegidas
-function autenticacaoMiddleware(req, res, next) {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    res.status(401).send('Token não fornecido');
-  } else {
-    jwt.verify(token, 'chave-secreta-do-token', (err, decoded) => {
-      if (err) {
-        res.status(401).send('Token inválido');
-      } else {
-        req.usuario = decoded;
-        next();
-      }
-    }
-)}
-
-};
-
+app.listen("3004", () => {
+  console.log("Servidor iniciado na porta 3004");
+});
